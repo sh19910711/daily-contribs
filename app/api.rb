8a33752f095bbd48c175a69dfe19568ef408c3c2
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-24 19:09:06 +0000

    app: add GET:/api/user/<id>/commits

--- app/api.rb
+++ app/api.rb
@@ -13,5 +13,11 @@ class API < Grape::API
     get do
       User.select(:id, :name, :github_id).all
     end
+
+    route_param :name do
+      get :commits do
+        User.select(:id, :name, :commits).find_by(name: params[:name])
+      end
+    end
   end
 end
