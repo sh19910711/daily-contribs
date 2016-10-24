01879362afe5765670dc05f669cad264ad6acfef
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-24 19:29:57 +0000

    webpack: set vue-loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   entry: './main',
   output: { path: __dirname + '/../app/public/js', filename: 'bundle.js' },
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.min.js',
+      'vue-router': 'vue-router/dist/vue-router.min.js'
+    },
+  },
   module: {
     loaders: [
+      { test: /\.vue$/, loader: 'vue' },
       { test: /\.js$/,  loader: 'babel', exclude: /node_modules/ },
     ]
   },
