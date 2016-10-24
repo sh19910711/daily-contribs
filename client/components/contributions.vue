c71cbd6907f1d423f5387345ec12944e80fdeff4
Hiroyuki Sano <sh19910711@gmail.com>
2016-10-24 20:02:17 +0000

    components: add contributions to show contributions graph

---
+++ client/components/contributions.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+  div
+    p {{commits.length}} contributions
+</template>
+
+<script>
+  export default {
+    name: 'contributions',
+    data() {
+      return { commits: [] };
+    },
+    created() {
+      api.commits('sh19910711').then(res => {
+        this.commits = res.commits;
+      });
+    }
+  }
+</script>
